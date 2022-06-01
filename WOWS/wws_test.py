e1e2700df1154b78ad1c2294838bde65f9675553
evro <evvro30@yandex.ru>
2022-06-01 18:59:06 +0300

    add b

--- WOWS/wws_test.py
+++ WOWS/wws_test.py
@@ -1,4 +1,5 @@
 # просто так(проверка заливки в Git) - возведение в степень числа
 a = lambda x, y: x ** y if x <= y else x - y
-# print(a(int(input()), int(input())))
-print(126*10*2)
+print(a(int(input()), int(input())))
+b = int(input('введи число \n'))
+print(b % 3)
